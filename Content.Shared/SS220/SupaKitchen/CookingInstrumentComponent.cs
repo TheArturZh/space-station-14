b4469301dd1e6e427b80a403937be7becd3d560a
Artur Zhidkov <thearturzh@gmail.com>
2023-08-28 17:16:48 +0300

    Cooking machine implementation start

---
+++ Content.Shared/SS220/SupaKitchen/CookingInstrumentComponent.cs
@@ -0,0 +1,14 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Shared.SS220.SupaKitchen;
+public sealed partial class CookingInstrumentComponent : Component
+{
+    [DataField("instrumentType", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<InstrumentTypePrototype>))]
+    [ViewVariables]
+    public string? InstrumentType;
+
+    [ViewVariables]
+    [DataField("ignoreTime")]
+    public bool IgnoreTime = false;
+}
