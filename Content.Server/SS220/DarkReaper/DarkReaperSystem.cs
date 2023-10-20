e50dd251fd5ddadc51e3d1832c687988a350d026
TheArturZh <thearturzh@gmail.com>
2023-10-20 03:39:15 +0300

    Added gameRule and CLA header

--- Content.Server/SS220/DarkReaper/DarkReaperSystem.cs
+++ Content.Server/SS220/DarkReaper/DarkReaperSystem.cs
@@ -1,5 +1,5 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Server.Actions;
-using Content.Server.Body.Components;
 using Content.Server.Ghost;
 using Content.Server.Light.Components;
 using Content.Server.Light.EntitySystems;
