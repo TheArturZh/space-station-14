506c40fcba37fc259537bad969e398bb68077abf
TheArturZh <thearturzh@gmail.com>
2023-11-26 16:06:22 +0300

    fix tests

--- Content.Server/SS220/SupaKitchen/CookingMachineSystem.cs
+++ Content.Server/SS220/SupaKitchen/CookingMachineSystem.cs
@@ -1,5 +1,4 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
-using Content.Server.Chemistry.EntitySystems;
 using Content.Server.Construction;
 using Content.Server.Hands.Systems;
 using Content.Server.Power.Components;
