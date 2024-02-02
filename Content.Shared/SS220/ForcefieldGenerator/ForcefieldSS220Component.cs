b766dcaef070fa82e932d12d4e778718dc79022e
TheArturZh <thearturzh@gmail.com>
2024-02-02 04:31:49 +0300

    save work

---
+++ Content.Shared/SS220/ForcefieldGenerator/ForcefieldSS220Component.cs
@@ -0,0 +1,17 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.SS220.ForcefieldGenerator;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class ForcefieldSS220Component : Component
+{
+    [ViewVariables(VVAccess.ReadOnly), AutoNetworkedField]
+    public EntityUid? Generator;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public SoundSpecifier HitSound = new SoundPathSpecifier("/Audio/SS220/Effects/shield/eshild_hit.ogg", new()
+    {
+        Volume = 1.25f
+    });
+}
