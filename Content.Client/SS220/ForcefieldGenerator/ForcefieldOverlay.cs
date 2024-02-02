e56cb8b4006237de86e394bf5ff05840c9a7c111
TheArturZh <thearturzh@gmail.com>
2024-02-02 04:37:04 +0300

    Add CLAs

--- Content.Client/SS220/ForcefieldGenerator/ForcefieldOverlay.cs
+++ Content.Client/SS220/ForcefieldGenerator/ForcefieldOverlay.cs
@@ -1,6 +1,6 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using System.Numerics;
 using Content.Shared.SS220.ForcefieldGenerator;
-using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
