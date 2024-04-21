aea21dc8b111677e4e392d8b95c37135516be32e
TheArturZh <thearturzh@gmail.com>
2024-04-21 21:40:43 +0300

    Only patch once

--- Content.Server/SS220/EnginePatches/PatchManager.cs
+++ Content.Server/SS220/EnginePatches/PatchManager.cs
@@ -8,13 +8,21 @@ public sealed class Patcher
     public static readonly bool HARMONY_LOGGING = false;
     public static readonly bool HARMONY_DEBUG = false;
 
+
+    public static bool PatchApplied { get; private set; } = false;
+
     public static void Patch(ILogManager logMan)
     {
+        if (PatchApplied)
+            return;
+
         Harmony.DEBUG = HARMONY_DEBUG;
         var sawmill = logMan.GetSawmill("Harmony");
         sawmill.Info("Applying Harmony patches...");
         var harmony = new Harmony("net.ss220.server.enginepatch");
         var assembly = Assembly.GetExecutingAssembly();
         harmony.PatchAll(assembly);
+        sawmill.Info("Harmony patches applied");
+        PatchApplied = true;
     }
 }
